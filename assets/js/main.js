6e90a7f01186518d8f3a4844fb5e3beab9d3be24
Yonesky Simamora <yonesskysimamora@gmail.com>
2024-07-02 16:20:11 +0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,7 +77,7 @@ sr.reveal(`.swiper-button-prev, .swiper-button-next`, { origin: "bottom" });
 
 /*======================= CARD POPUP JS ======================*/
 const modal = document.querySelectorAll(".modal"),
-  cardBtn = document.querySelectorAll(".card__product"),
+  cardBtn = document.querySelectorAll(".home__article"),
   modalClose = document.querySelectorAll(".modal__close"),
   modalCard = document.querySelectorAll(".modal__card");
 
