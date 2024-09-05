f5c61486aba7c3c53bce551361a98fb65ea67de6
Yonesky Simamora <yonesskysimamora@gmail.com>
2024-09-05 16:03:40 +0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,31 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+// const navMenu = document.getElementById("nav-menu"),
+//   navToggle = document.getElementById("nav-toggle"),
+//   navClose = document.getElementById("nav-close");
 
-// Menu Show
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
-}
+// // Menu Show
+// if (navToggle) {
+//   navToggle.addEventListener("click", () => {
+//     navMenu.classList.add("show-menu");
+//   });
+// }
 
-// Menu Hidden
-if (navClose) {
-  navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show-menu");
-  });
-}
+// // Menu Hidden
+// if (navClose) {
+//   navClose.addEventListener("click", () => {
+//     navMenu.classList.remove("show-menu");
+//   });
+// }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+// const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () => {
-  const navMenu = document.getElementById('nav-menu')
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu')
-}
-navLink.forEach((n) => n.addEventListener('click', linkAction))
+// const linkAction = () => {
+//   const navMenu = document.getElementById('nav-menu')
+//   // When we click on each nav__link, we remove the show-menu class
+//   navMenu.classList.remove('show-menu')
+// }
+// navLink.forEach((n) => n.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
 const shadowHeader = () => {
@@ -48,25 +48,25 @@ window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () => {
-  const scrollDown = window.scrollY
+// const scrollActive = () => {
+//   const scrollDown = window.scrollY
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop - 58,
-          sectionId = current.getAttribute('id'),
-          sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-          // If section top is less than the position it adds class 'active'
-          if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add('active-link')
-          } else {
-            sectionsClass.classList.remove('active-link')
-          }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//   sections.forEach((current) => {
+//     const sectionHeight = current.offsetHeight,
+//           sectionTop = current.offsetTop - 58,
+//           sectionId = current.getAttribute('id'),
+//           sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+//           // If section top is less than the position it adds class 'active'
+//           if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+//             sectionsClass.classList.add('active-link')
+//           } else {
+//             sectionsClass.classList.remove('active-link')
+//           }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 /*=============== SWIPER JANGAN MENCURI===============*/
 let swiperHome = new Swiper(".home__swiper", {
