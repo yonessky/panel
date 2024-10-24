50297caab17aed2eba69f5f4d12ee9ff476e8991
Yonesky Simamora <yonesskysimamora@gmail.com>
2024-10-24 16:53:56 +0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,6 +168,35 @@ window.addEventListener("resize", () => {
     menu.style.setProperty("--timeOut", "none");
 });
 
+/*=============== VALUE ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.value__accordion-item')
+
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.value__accordion-header')
+
+    accordionHeader.addEventListener('click', () =>{
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(openItem && openItem!== item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem =  (item) => {
+    const accordionContent = item.querySelector('.value__accordion-content')
+    
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
+
 /*==========   counterUp  ==========*/
 $(".counter").counterUp({
   delay: 10,
